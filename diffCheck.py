80f32362a10f6612386b0879921b27c05a3b57c2
tuang <hau.mun.tuang@pi-pe.co.jp>
2019-04-19 11:19:53 +0900

    for mr.tsuda

--- diffCheck.py
+++ diffCheck.py
@@ -10,11 +10,12 @@ if not len(sys.argv) == 3:
 tar1 = sys.argv[1]
 tar2 = sys.argv[2]
 
+#　○○なら変更点ないと決めるため
 standard_diff = 2.0
 
 print('checking ...............')
 
-
+# 2つのtarの中身（構成）を辞書型に入れる
 def create_dict(extractedTar, targetDict):
 	for idx, data in enumerate(extractedTar.getmembers()):
 		targetDict.update(
@@ -25,11 +26,12 @@ def create_dict(extractedTar, targetDict):
 				}
 			}
 		)
-
-		if targetDict[data.name]['type'] == '' and targetDict[data.name]['size'] == 0:
-			del targetDict[data.name]
+		
+		#if targetDict[data.name]['type'] == '' and targetDict[data.name]['size'] == 0:
+		#	del targetDict[data.name]
 
 def is_same(arg_1, arg_2):
+	# 2っのdictにあるkey(file path)をunionする
 	union_keys = set().union(list(arg_1.keys()), arg_2.keys())
 	for union_key in union_keys:
 		if arg_1.get(union_key) == None:
